54376e299cd69c1b427e114410ca8e41047d498b
cliff_kuang <cliffkuang@gmail.com>
2017-04-02 16:06:56 -0400

    added new libraries

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 $(function() {
 
-  $('#first-text-fade-in').waypoint (function() {
-    console.log ('Waypoint!');
-    $('#first-text-fade-in').addClass('post_text-box--fade-in');
+    $('#first-text-fade-in').waypoint(function() {
+        console.log('Waypoint!');
+        $('#first-text-fade-in').addClass('post_text-box--fade-in');
 
-  },{
-    offset: '50%'
+    }, {
+        offset: '50%'
 
-  });
+    });
 
-  $('#second-text-fade-in').waypoint (function() {
-  console.log ('Waypoint!');
-  $('#second-text-fade-in').addClass('post_text-box--fade-in');
+    $('#second-text-fade-in').waypoint(function() {
+        console.log('Waypoint!');
+        $('#second-text-fade-in').addClass('post_text-box--fade-in');
 
-  },{
-    offset: '50%'
+    }, {
+        offset: '50%'
 
-  });
+    });
 });
