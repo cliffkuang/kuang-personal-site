26dc533cffe9f6ae125323fcddd662eab542de47
cliff_kuang <cliffkuang@gmail.com>
2017-04-02 14:27:26 -0400

    Added JS, started on SM and MD breakpoints

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function() {
+
+  $('#first-text-fade-in').waypoint (function() {
+    console.log ('Waypoint!');
+    $('#first-text-fade-in').addClass('post_text-box--fade-in');
+
+  },{
+    offset: '50%'
+
+  });
+
+  $('#second-text-fade-in').waypoint (function() {
+  console.log ('Waypoint!');
+  $('#second-text-fade-in').addClass('post_text-box--fade-in');
+
+  },{
+    offset: '50%'
+
+  });
+});
